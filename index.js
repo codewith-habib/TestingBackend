7e167d4491b9acc9bbbc7350197b818b3e9a4b43
Muhammad Habib <habib80246@gmail.com>
2024-07-09 11:37:58 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req,res)=>{
+    res.send('About Us')
+})
+
+app.get('/contact', (req,res)=>{
+    res.send('Contact Us')
+})
+
+app.get('/login', (req,res)=>{
+    res.send("You are login")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
